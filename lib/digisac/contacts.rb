7251e444a024b0dfcf8554e2fe8c1649460d9ff6
Douglas Lara <douglasdelaras@gmail.com>
2021-03-17 07:08:00 -0300

    Add custom params in search_contacts_by_number

--- lib/digisac/contacts.rb
+++ lib/digisac/contacts.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
+# Contacts actions
 class Digisac
-  def search_contacts_by_number(number)
+  def search_contacts_by_number(number, params = {})
     number_last_chars = number.chars.last(8).join
-    get_request("/v1/contacts", { 'where[data.number][$iLike]': "%#{number_last_chars}%" })
+    params.merge!({ 'where[data.number][$iLike]': "%#{number_last_chars}%" })
+
+    get_request("/v1/contacts", params)
   end
 end
--- spec/digisac/contacts_spec.rb
+++ spec/digisac/contacts_spec.rb
@@ -9,5 +9,12 @@ RSpec.describe Digisac do
     it "success" do
       expect(subject.search_contacts_by_number(ENV["TEST_NUMBER"])).to include(:ok)
     end
+
+    it "success with custom params" do
+      request = subject.search_contacts_by_number(ENV["TEST_NUMBER"], { 'include[0]': "service", 'include[1]': "tags" })
+      expect(request).to include(:ok)
+      expect(request[:ok]["data"][0]).to include("service")
+      expect(request[:ok]["data"][0]).to include("tags")
+    end
   end
 end
