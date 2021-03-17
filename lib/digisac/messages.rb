d3e38abfe74abde0b8f0b576d6ccb1e8efd26c4d
Douglas Lara <douglasdelaras@gmail.com>
2021-03-17 18:26:37 -0300

    Get contact messages

--- lib/digisac/messages.rb
+++ lib/digisac/messages.rb
@@ -12,4 +12,17 @@ class Digisac
 
     post_request("/v1/messages", body_params)
   end
+
+  def get_contact_messages(contact_id, params = nil)
+    url_params = {
+      'where[contactId]': contact_id,
+      'where[visible]': true,
+      'limit': 20,
+      'order[0][0]': "timestamp",
+      'order[0][1]': "DESC"
+    }
+    url_params.merge!(params) unless params.nil?
+
+    get_request("/v1/messages", url_params)
+  end
 end
--- spec/digisac/messages_spec.rb
+++ spec/digisac/messages_spec.rb
@@ -19,4 +19,19 @@ RSpec.describe Digisac do
       expect(subject.send_message_to_contact_id("f8d0e7af-cbca-4353-8610-bb4f9dee7f66", "Hello")).to include(:error)
     end
   end
+
+  describe "#get_contact_messages", :vcr do
+    it "success" do
+      request = subject.get_contact_messages("9c0007c1-83a5-4d1d-a96e-ee164b1b8f62")
+      expect(request).to include(:ok)
+      expect(request[:ok]["data"].count).to eq(15)
+    end
+
+    it "with filter" do
+      request = subject.get_contact_messages("9c0007c1-83a5-4d1d-a96e-ee164b1b8f62",
+                                             { 'where[createdAt]': "2021-03-03T19:26:44.307Z" })
+      expect(request).to include(:ok)
+      expect(request[:ok]["data"].count).to eq(1)
+    end
+  end
 end
