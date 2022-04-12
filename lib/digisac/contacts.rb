72bf2bdce6499ccebb4b25ccdd82dd6ccc27bd06
Karina Almeida <s.karinaalmeida@gmail.com>
2022-04-12 15:39:29 -0300

    removed data verification on create contact

--- lib/digisac/contacts.rb
+++ lib/digisac/contacts.rb
@@ -6,7 +6,7 @@ class Digisac
     params = params.transform_keys(&:to_sym) if params != {}
 
     number_last_chars = number.chars.last(8).join
-    params.merge!({ 'where[data.number][$iLike]': "%#{number_last_chars}%" })
+    params.merge!({ 'where[data.number][$iLike]': "%#{number_last_chars}" })
 
     get_request("/v1/contacts", params)
   end
@@ -17,7 +17,7 @@ class Digisac
 
     request = post_request("/v1/contacts", params)
 
-    if request.key?(:ok) && request[:ok]["data"]["valid"] != false
+    if request.key?(:ok)
       { ok: request[:ok] }
     else
       { error: request[:ok] }
